13e84aef0aeb7e7f59dc08811cb5e0d3ee45313c
NandavardhanRadhakrishnan <93426172+NandavardhanRadhakrishnan@users.noreply.github.com>
2024-02-28 09:28:57 +0530

    Update main.js

--- Week 4/job registration/main.js
+++ Week 4/job registration/main.js
@@ -3,17 +3,25 @@ function validate(){
     var empPass = document.getElementById("employeePassword").value
     var empEmail = document.getElementById("employeeEmail").value
     var empPhone = document.getElementById("employeePhone").value
-
+    var valid = true
     if(! /^[A-Za-z ]+$/.test(empName)){
         alert("Only insert letters and whitespace in name")
+        valid=false
     }
     if(empPass.length < 6){
         alert("Password should be 6 or more characters")
+        valid=false
     }
     if(! /^\S+@\S+\.\S+$/.test(empEmail)){
         alert("Email not valid")
+        valid=false
     }
     if(! /^\d{10}$/.test(empPhone)){
         alert("Phone number not valid enter 10 digits only")
-    }    
+        valid=false
+    }
+    if(valid){
+        alert("Form valid")
+    }
+
 }
