06d6fe925402911d772da6213377b633c98997fa
NandavardhanRadhakrishnan <93426172+NandavardhanRadhakrishnan@users.noreply.github.com>
2024-03-06 09:42:08 +0530

    Add files via upload

--- Week 6/loadFromJson/script.js
+++ Week 6/loadFromJson/script.js
@@ -2,7 +2,11 @@ function loadData(){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
-            document.getElementById("tableData").innerHTML = this.responseText;
+            var table = document.getElementById("tableData");
+            var jsonData = JSON.parse(this.responseText);
+            jsonData.forEach(element => {
+                table.innerHTML += ("<tr><td>"+element.modelNumber+"</td><td>"+element.name+"</td><td>"+element.price+"</td><td><img src="+element.img+"></td></tr>");
+            });
         }
     };
     xhttp.open("GET","data.json",true);
