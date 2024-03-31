449bbc8a6fd254baf9fd2cd83b231bfb321dccc2
NandavardhanRadhakrishnan <nr3648@srmist.edu.in>
2024-03-31 20:46:07 +0530

    added week 8

--- Week 7/src/main/java/com/servlet/loginServlet.java
+++ Week 7/src/main/java/com/servlet/loginServlet.java
@@ -6,19 +6,33 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
 
 public class loginServlet extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 		response.setContentType("text/html");
-		String usrName = request.getParameter("inTxtBox");
-		PrintWriter obj = response.getWriter();
-		if(usrName.equals("Nanda")) {			
-			obj.println("<h1>welcome "+usrName+"</h1>");
+		String username = request.getParameter("usernameTextbox");
+		String password = request.getParameter("passwordTextbox");
+		PrintWriter out = response.getWriter();
+		
+		try {
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/wdd", "root", "admin");
+			PreparedStatement ps = con.prepareStatement("select * from exp7 where username=? and password=?");
+			ps.setString(1, username);
+			ps.setString(2, password);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				out.println("Login succesful. Welcome:"+username);
+			} else {
+				out.println("Invalid username or password");
+			}
+			
+			
+		} catch (Exception e) {
+			out.println(e);
 		}
-		else {
-			obj.println("<h1>Not valid user"+usrName+"</h1>");
-		}
-		obj.close();
-		}
+	}		
 }
